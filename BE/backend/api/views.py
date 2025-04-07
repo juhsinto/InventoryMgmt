5e5d3887e2fcaf73f7d4be568fb3adb2307b8157
jacinto93 <jmendes@greeningaustralia.org.au>
2025-04-08 00:43:05 +1000

    be - added a RootIndex - default view

--- BE/backend/api/views.py
+++ BE/backend/api/views.py
@@ -3,7 +3,13 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny
+from django.http import HttpResponse
+from datetime import datetime
 
+def RootIndex(request):
+    now = datetime.now()
+    html = '<html lang="en"><body>This is the default view. The UTC time is %s.</body></html>' % now
+    return HttpResponse(html)
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
